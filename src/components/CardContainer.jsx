eaca2f51d64ba316fc1d0db24126f1c2949d65b1
Prem Soni <premsoni0469@gmail.com>
2025-02-08 14:07:17 +0530

    Optimized the code for CardContainer and RestaurantCard

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,16 +1,90 @@
 import RestaurantCard from "./RestaurantCard";
 
 function CardContainer(){
+
+    const restaurantData = [
+        {
+            name:"Chinese Wok",
+            rating:"4.2",
+            deliveryTime:"25-30 mins",
+            cuisines:"Chinese, Thai, Mexican",
+            location:"Bandra Kurla Complex"
+        }, 
+        
+        {
+            name:"Pizza Hut",
+            rating:"4.3",
+            deliveryTime:"25-28 mins",
+            cuisines:"Pizza, Fast Food",
+            location:"Santacruz"
+        }, 
+
+        {
+            name:"Naturals",
+            rating:"4.6",
+            deliveryTime:"15-20 mins",
+            cuisines:"Dessert, Ice Cream",
+            location:"Ghatkopar"
+        }, 
+
+        {
+            name:"McDonalds",
+            rating:"4.0",
+            deliveryTime:"30-35 mins",
+            cuisines:"Burger, Pizza, French",
+            location:"Chembur"
+        }, 
+        
+    ] // It is an array of objects of restaurants
+
     return(
         <>
             <div className="flex flex-wrap justify-center gap-4 md,lg:px-56 px-4 py-14">
+               {
+                restaurantData.map((restaurant) => {       // restaurant is an object here
+                    return <RestaurantCard 
+                    {...restaurant} // using spread operator
+
+                    
+                    // name={restaurant.name}  // Remember, the property name should be the same as the one present in the RestaurantCard Component
+                    // rating={restaurant.rating}
+                    // deliveryTime={restaurant.deliveryTime}
+                    // cuisines={restaurant.cuisines}
+                    // location={restaurant.location} // The same things can be written as location = {restaurant["location"]}. This thing will be more helpful in cases where the key in our data is not a proper string, i.e. it starts from a specil character, like @psp. We can't write restaurant.@psp, it will give an error, instead we can write as restaurant["@psp"].    
+
+                    // You can also change the order of these 5 properties, but it won't affect the data, means where is should be displayed, it will, but the only differemce will be the order in which we write in the code.
+
+                    />
+                })
+               }
+               
+               
+               
+               
+                {/* <RestaurantCard 
+                name="Chinese Wok" 
+                rating="4.2" 
+                deliveryTime="25-30 mins" 
+                cuisines="Chinese, Thai, Mexican" 
+                location="Bandra Kurla Complex"/>
+
+                <RestaurantCard 
+                name="Pizza Hut" 
+                rating="4.3" 
+                deliveryTime="25-28 mins" 
+                cuisines="Pizza, Fast Food" 
+                location="Santacruz"/>
+
+                <RestaurantCard 
+                name="Naturals" 
+                rating="4.6" 
+                deliveryTime="15-20 mins" 
+                cuisines="Dessert, Ice Cream" 
+                location="Ghatkopar"/>
+
                 <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                <RestaurantCard /> */}
+                
             </div>
         </>
     );
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,6 +1,10 @@
 import Pizza from '../assets/pizza.avif'
 
-function RestaurantCard(){
+function RestaurantCard({name, rating, deliveryTime, cuisines, location}){
+    // console.log(props) // This line should be commented if you are passing all the props as argument 
+
+    // const {name, rating, deliveryTime, cuisines, location} = props
+
     return(
         <>
             <div>
@@ -8,15 +12,24 @@ function RestaurantCard(){
                     {/* <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/c08ef470-8561-436d-81ec-b119dfc997fa_9862.jpg" alt="Pizza" className='w-full h-full object-cover rounded-[20px]' /> */}
                     <img src={Pizza} alt="Pizza" className='w-full h-full object-cover rounded-[20px]' />
                 </div>
-                <div className='text-[18px] pt-2'>
-                    <p className='font-bold px-2'>Pizza Hut</p>
+                {/* <div className='text-[18px] pt-2'>
+                    <p className='font-bold px-2'>{props.name}</p>
                     <div className='flex font-semibold gap-2'> 
-                        <p>⭐ 4.2</p>
+                        <p>⭐ {props.rating}</p>
                         <p>•</p>
-                        <p>25-30 mins</p>
+                        <p>{props.deliveryTime}</p>
                     </div>
-                    <p className='text-gray-600 px-2'>Pizzas</p>
-                    <p className='text-gray-600 px-2'>Bandra Kurla Complex</p>
+                    <p className='text-gray-600 px-2'>{props.cuisines}</p>
+                    <p className='text-gray-600 px-2'>{props.location}</p> */}
+                <div className='text-[18px] pt-2'>
+                    <p className='font-bold px-2'>{name}</p>
+                    <div className='flex font-semibold gap-2'> 
+                        <p>⭐ {rating}</p>
+                        <p>•</p>
+                        <p>{deliveryTime}</p>
+                    </div>
+                    <p className='text-gray-600 px-2'>{cuisines}</p>
+                    <p className='text-gray-600 px-2'>{location}</p>
                 </div>
             </div>
         </>
