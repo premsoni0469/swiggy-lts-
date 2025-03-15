33946d55def5e6062a471525421153ab32fcbb78
Prem Soni <premsoni0469@gmail.com>
2025-03-15 14:50:10 +0530

    little bit visual changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header(){
         <>
             <div className="flex justify-between shadow-md items-center md:px-20 py-4">
                 <a className="flex items-center" href="#">
-                    <img src={Logo} alt="logo" className="w-16" /><span className="font-bold text-[1.7rem]">Smiggy</span>
+                    <img src={Logo} alt="logo" className="w-16" /><span className="font-bold text-2xl">Smiggy</span>
                 </a>
                 <div className="flex gap-7 font-medium">
                     <a href="#">Home</a>
