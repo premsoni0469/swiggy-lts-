5116084903f24944b3b8b0ffdd58b1db7aefe2a7
Prem Soni <premsoni0469@gmail.com>
2025-03-17 20:35:07 +0530

    completed all pages from header

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -12,7 +12,7 @@ const ContactUs = () => {
           ></iframe>
         </div>
         <div className="px-5 py-24 mx-auto flex">
-          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md mx-16">
+          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-3 md:mt-0 relative z-10 shadow-md md:mx-16 mx-18">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
               Something to say?
             </h2>
