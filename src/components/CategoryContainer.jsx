1dbfea654485530762286f511ce0087700af281c
Prem Soni <premsoni0469@gmail.com>
2025-03-24 21:08:29 +0530

    Added accordion in Menu page

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -1,30 +1,54 @@
-import DishDetails from "./DishDetails"
-// This component contains all the dishes present inside each category, like recommended, Veg Pizza, etc
-const CategoryContainer = ({categoryTitle, count, collection}) => {
-    return (
-        <>
-            <div>
-                <p className="pb-5">
-                    <span className="font-bold text-xl">{categoryTitle} ({count})</span>
-                </p>
-                <div>
-                    {
-                        collection.map(foodItem => {
-                            return <DishDetails 
-                            key={foodItem?.info?.id} 
-                            imgURL={foodItem?.card?.info?.imageId}
-                            isVeg={foodItem?.card?.info?.isVeg}
-                            title={foodItem?.card?.info?.name}
-                            price={foodItem?.card?.info?.defaultPrice/100 || foodItem?.card?.info?.price/100}
-                            avgRating={foodItem?.card?.info?.ratings?.aggregatedRating?.rating || "Be the first one to try and review"}
-                            ratingCountV2={foodItem?.card?.info?.ratings?.aggregatedRating?.ratingCountV2 || "0"}
-                            description={foodItem?.card?.info?.description} />
-                        })
-                    }
-                </div>
-            </div>
-        </>
-    )
-}
+import { useState } from "react";
+import DishDetails from "./DishDetails";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
-export default CategoryContainer
+const CategoryContainer = ({ categoryTitle, count, collection }) => {
+  const [isActive, setIsActive] = useState(false); // State to toggle accordion
+
+  return (
+    <>
+      <div className="border-b border-gray-300">
+        <div
+          className="py-5 flex justify-between cursor-pointer"
+          onClick={() => setIsActive(!isActive)} // Toggle accordion on click
+        >
+          <p>
+            <span className="font-bold text-xl">
+              {categoryTitle} ({count})
+            </span>
+          </p>
+          <div>
+            {isActive ? <ChevronUp /> : <ChevronDown />}
+          </div>
+        </div>
+        {isActive && ( // Conditionally render content if accordion is active
+          <div className="pl-5">
+            {collection.map((foodItem) => (
+              <DishDetails
+                key={foodItem?.info?.id}
+                imgURL={foodItem?.card?.info?.imageId}
+                isVeg={foodItem?.card?.info?.isVeg}
+                title={foodItem?.card?.info?.name}
+                price={
+                  foodItem?.card?.info?.defaultPrice / 100 ||
+                  foodItem?.card?.info?.price / 100
+                }
+                avgRating={
+                  foodItem?.card?.info?.ratings?.aggregatedRating?.rating ||
+                  "Be the first one to try and review"
+                }
+                ratingCountV2={
+                  foodItem?.card?.info?.ratings?.aggregatedRating
+                    ?.ratingCountV2 || "0"
+                }
+                description={foodItem?.card?.info?.description}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default CategoryContainer;
