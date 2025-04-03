1887523601226121a14e6032cdc44d6d92e330a0
Prem Soni <premsoni0469@gmail.com>
2025-04-03 10:34:25 +0530

    Updated arrow buttons

--- src/components/FoodItemsCarousel.jsx
+++ src/components/FoodItemsCarousel.jsx
@@ -18,20 +18,30 @@ function FoodItemsCarousel() {
     <>
       <div className="font-bold flex justify-between text-xl mx-16 lg:mx-40 md:mx-24 pt-8 pb-2">
         <p>What's on your mind?</p>
-        <div className="flex lg:mx-14 gap-3 lg:gap-10">
-          <button className="scale-105 lg:scale-150">
-            <CircleChevronLeft className="bg-gray-200 rounded-xl" onClick={slideLeft}/>
+        <div className="flex lg:mx-14 gap-3 lg:gap-6">
+          <button className="bg-gray-200 rounded-full p-1" onClick={slideLeft}>
+            <svg width="25" height="25" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+              <path d="M34 20H10m0 0l9-9m-9 9l9 9" stroke="#222" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
           </button>
-          <button className="scale-105 lg:scale-150">
-            <CircleChevronRight className="bg-gray-200 rounded-xl" onClick={slideRight}/>
+          <button className="bg-gray-200 rounded-full p-1" onClick={slideRight}>
+            <svg width="25" height="25" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" >
+              <path d="M6 20h24m0 0l-9-9m9 9l-9 9" stroke="#222" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
           </button>
         </div>
       </div>
-      <div className="w-9/12 m-auto overflow-x-auto custom-scrollbar scroll-smooth" id="slider">
+      <div
+        className="w-9/12 m-auto overflow-x-auto custom-scrollbar scroll-smooth"
+        id="slider"
+      >
         <div className="flex gap-4">
           {foodItemsCarousel.map((item) => (
-            <div className="lg:w-36 min-w-[95px] lg:min-w-[150px]" key={item?.id}>
-              <img src={item?.img}/>
+            <div
+              className="lg:w-36 min-w-[95px] lg:min-w-[150px]"
+              key={item?.id}
+            >
+              <img src={item?.img} alt={item?.name || "Food Item"} />
             </div>
           ))}
         </div>
--- src/components/RestaurantBanner.jsx
+++ src/components/RestaurantBanner.jsx
@@ -75,13 +75,17 @@ const RestaurantBanner = ({title, avgRating, totalRatingsString, costForTwoMessa
                 </div>
                 <div className="font-bold flex justify-between lg:justify-between text-xl px-5 lg:mx-5 mt-10 mb-5">
                     <p>Deals for you</p>
-                    <div className="flex lg:mx-14 gap-10">
-                    <button className="scale-150">
-                        <CircleChevronLeft className="bg-gray-200 rounded-xl" onClick={slideLeft}/>
-                    </button>
-                    <button className="scale-150">
-                        <CircleChevronRight className="bg-gray-200 rounded-xl" onClick={slideRight}/>
-                    </button>
+                    <div className="flex lg:mx-14 gap-5">
+                        <button className="bg-gray-200 rounded-full p-1">
+                            <svg width="25" height="25" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M34 20H10m0 0l9-9m-9 9l9 9" stroke="#222" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <button className="bg-gray-200 rounded-full p-1">
+                            <svg width="25" height="25" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6 20h24m0 0l-9-9m9 9l-9 9" stroke="#222" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
                     </div>
                 </div>
                 
